a1bbcd96979c275706d7f443f9df62e9963e07fb
Vincent Carlino <vjcarlino22@gmail.com>
2020-02-12 22:23:13 -0500

    Add styled components, create basic type styled components, add css reset

---
+++ src/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import './index.css';
+
+const H1 = styled.h1`
+    font-family: "Amaranth", sans-serif;
+    font-size: 72px;
+`;
+
+const H2 = styled.h2`
+    font-family: "Poppins", sans-serif;
+    font-size: 36px;
+`;
+
+const H3 = styled.h3`
+    font-family: "Poppins", sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+
+`;
+
+const H4 = styled.h4`
+
+`;
+
+const H5 = styled.h5`
+
+`;
+
+const P = styled.p`
+    font-family: "Poppins";
+    font-size: 18px;
+
+`;
+
+export { H1, H2, H3, H4, H5, P};
