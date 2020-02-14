51bc13ef12e7d805b466cf3c235928a4c0031564
Tina Wu <tinawu0603@hotmail.com>
2020-02-14 17:31:02 -0500

    added shopify-buy client

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import { H1, H2, H3, P } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <H1>This is an H1</H1>
-        <H2>This is an H2</H2>
-        <H3>This is an H3</H3>
-        <P>This is a P</P>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+      shop: {}
+    };
+  }
+
+  componentDidMount() {
+    this.props.client.product.fetchAll().then((res) => {
+      console.log("fetching all products");
+      console.log(res);
+      this.setState({
+        products: res,
+      });
+      console.log(this.state.products);
+    });
+
+    this.props.client.shop.fetchInfo().then((res) => {
+      this.setState({
+        shop: res,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <H1>This is an H1</H1>
+          <H2>This is an H2</H2>
+          <H3>This is an H3</H3>
+          <P>This is a P</P>
+        </header>
+        {
+          this.state.products.map((product, index) => {
+            return (
+              <h3>{product}</h3>
+            )
+          })
+        }
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Client from 'shopify-buy';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+require('dotenv').config()
+
+const client = Client.buildClient({
+    storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_TOKEN,
+    domain: 'better-than-belts.myshopify.com'
+});
+
+console.log(process.env.SHOPIFY_STOREFRONT_TOKEN)
+
+ReactDOM.render(<App client={client} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
