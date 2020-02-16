2bd9b939188aabac99bd33af5372165b8e73a88e
Tina Wu <tinawu0603@hotmail.com>
2020-02-16 15:45:56 -0500

    moved shopify client to new component and initialize in app not index

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
 import React from 'react';
-import { H1, H2, H3, P } from './styles';
+import ShopifyClient from './components/ShopifyClient';
+import Client from 'shopify-buy';
 
-class App extends React.Component {
-  constructor() {
-    super();
+function App() {
+  // Set up Shopify client for BTB
+  const client = Client.buildClient({
+    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_STOREFRONT_TOKEN,
+    domain: 'better-than-belts.myshopify.com'
+  });
 
-    this.state = {
-      products: [],
-      shop: {}
-    };
-  }
-
-  componentDidMount() {
-    this.props.client.product.fetchAll().then((res) => {
-      this.setState({
-        products: res,
-      });
-    });
-
-    this.props.client.shop.fetchInfo().then((res) => {
-      this.setState({
-        shop: res,
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          {/* <H1>This is an H1</H1>
-          <H2>This is an H2</H2>
-          <H3>This is an H3</H3>
-          <P>This is a P</P> */}
-        </header>
-        {
-          this.state.products.map((product, index) => {
-            return (
-              // product keys:
-              // {id, availableForSale, createdAt, updatedAt, descriptionHtml, description, handle, productType, title, vendor, publishedAt, onlineStoreUrl, options, images, variants, refetchQuery, type, nextPageQueryAndPath, hasNextPage, hasPreviousPage, variableValues}
-              // product image keys:
-              // {id, src, altText, type, nextPageQueryAndPath, hasNextPage, hasPreviousPage, variableValues}
-              <div>
-                <H1>Product: {product.title}</H1>
-                <H3>{product.productType}</H3>
-                <H3>{product.description}</H3>
-                <img src={product.images[0].src} width="100"></img>
-                <hr></hr>
-              </div>
-            )
-          })
-        }
-
-      </div >
-    );
-  }
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ShopifyClient client={client} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Client from 'shopify-buy';
+
 import * as serviceWorker from './serviceWorker';
 
-const client = Client.buildClient({
-    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_STOREFRONT_TOKEN,
-    domain: 'better-than-belts.myshopify.com'
-});
-
-ReactDOM.render(<App client={client} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
