679a281a707fbe744c6689c6846637523f4f07ae
Tina Wu <tinawu0603@hotmail.com>
2020-02-23 22:33:18 -0500

    added routes

---
+++ src/components/PageContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../views/Home';
+
+const PageContainer = (props) => {
+    return (
+        <div>
+            <Router>
+                <Route path="/"
+                    exact={true}
+                    render={() =>
+                        <Home {...props} />
+                    } />
+                <Route path="/shop"
+                    exact={true}
+                    render={() =>
+                        <Home {...props} />
+                    } />
+                <Route path="/our-product"
+                    exact={true}
+                    render={() =>
+                        <Home {...props} />
+                    } />
+                <Route path="/our-story"
+                    exact={true}
+                    render={() =>
+                        <Home {...props} />
+                    } />
+            </Router>
+        </div>
+    )
+};
+
+export default PageContainer;
