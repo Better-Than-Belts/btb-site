51d74ac36e13859b45fd67ef12a9d399b82f6875
Tina Wu <tinawu0603@hotmail.com>
2020-02-26 16:45:30 -0500

    remove mock image and use actual product image

--- src/components/PLP/PLPProduct.js
+++ src/components/PLP/PLPProduct.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { MockImagePLPProduct, H3 } from '../../styles';
 
 class PLPProduct extends React.Component {
     render() {
