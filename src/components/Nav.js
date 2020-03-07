d3c86efef83c3a23dac491db0577cd492129490a
Tina Wu <tinawu0603@hotmail.com>
2020-03-07 11:46:51 -0500

    change navbar-brand and cart icon location based on screensize

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,45 +4,96 @@ import styled from 'styled-components';
 import { Link, NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
-const NavRouter = (props) => {
-    return (
-        <>
-            <Navbar style={navBG} expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Link to={`/`}>
-                    <Navbar.Brand>
-                        <MockLogo />
-                    </Navbar.Brand>
-                </Link>
-                <Nav>
-                    <Nav.Link><MockCartIcon /></Nav.Link>
-                </Nav>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavLink to={`/shop`} style={navItem}>
-                            <NavText>Shop</NavText>
-                        </NavLink>
-                        <NavLink to={`/our-product`} style={navItem}>
-                            <NavText>Our Product</NavText>
-                        </NavLink>
-                        <NavLink to={`/our-story`} style={navItem}>
-                            <NavText>Our Story</NavText>
-                        </NavLink>
-                        <NavLink to={`/faq`} style={navItem}>
-                            <NavText>FAQ</NavText>
-                        </NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </>
+class NavRouter extends React.Component {
+    state = { width: 0 };
 
-    )
+    updateScreenSize = () => {
+        this.setState({ width: window.innerWidth });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateScreenSize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateScreenSize);
+    }
+
+    render() {
+        if (this.state.width > 768) {
+            // desktop
+            return (
+                <>
+                    <Navbar style={navBG} expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Link to={`/`}>
+                            <Navbar.Brand>
+                                <MockLogo />
+                            </Navbar.Brand>
+                        </Link>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <NavLink to={`/shop`} style={navItem}>
+                                    <NavText>Shop</NavText>
+                                </NavLink>
+                                <NavLink to={`/our-product`} style={navItem}>
+                                    <NavText>Our Product</NavText>
+                                </NavLink>
+                                <NavLink to={`/our-story`} style={navItem}>
+                                    <NavText>Our Story</NavText>
+                                </NavLink>
+                                <NavLink to={`/faq`} style={navItem}>
+                                    <NavText>FAQ</NavText>
+                                </NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                        <Nav>
+                            <Nav.Link><MockCartIcon /></Nav.Link>
+                        </Nav>
+                    </Navbar>
+                </>
+            )
+        } else {
+            // mobile
+            return (
+                <>
+                    <Navbar style={navBG} expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Link to={`/`}>
+                            <Navbar.Brand>
+                                <MockLogo />
+                            </Navbar.Brand>
+                        </Link>
+                        <Nav>
+                            <Nav.Link><MockCartIcon /></Nav.Link>
+                        </Nav>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <NavLink to={`/shop`} style={navItem}>
+                                    <NavText>Shop</NavText>
+                                </NavLink>
+                                <NavLink to={`/our-product`} style={navItem}>
+                                    <NavText>Our Product</NavText>
+                                </NavLink>
+                                <NavLink to={`/our-story`} style={navItem}>
+                                    <NavText>Our Story</NavText>
+                                </NavLink>
+                                <NavLink to={`/faq`} style={navItem}>
+                                    <NavText>FAQ</NavText>
+                                </NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </>
+            )
+        }
+    }
 }
 
 export default NavRouter;
 
 const navBG = {
-    'background-color': '#E2E2E2',
+    'backgroundColor': '#E2E2E2',
 };
 
 const navItem = {
