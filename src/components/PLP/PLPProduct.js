a835cb896d4e633db097f7b20612c92b4717db09
Tina Wu <tinawu0603@hotmail.com>
2020-03-07 14:30:05 -0500

    added product component in PLP page

--- src/components/PLP/PLPProduct.js
+++ src/components/PLP/PLPProduct.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { H3 } from '../../styles';
+import { device } from '../../device.js';
 
 class PLPProduct extends React.Component {
     render() {
         return (
-            <div className="col-sm-4" style={productStyle}>
+            <ProductDiv className="col-md-4 col-6">
                 {this.props.product.images.length ?
                     <img src={this.props.product.images[0].src} alt={this.props.product.title} width="100%" /> : null}
                 <ProductName>{this.props.product.title}</ProductName>
-            </div>
+            </ProductDiv>
         )
     }
 
@@ -19,9 +20,10 @@ export default PLPProduct;
 
 const ProductName = styled(H3)`
     font-weight: normal;
-    font-size: 20px;
+    font-size: 16px;
 `
 
-const productStyle = {
-    'padding': '0 40px'
-};
+const ProductDiv = styled.div`
+    padding-top: 10px;
+    padding-bottom: 10px;
+`
