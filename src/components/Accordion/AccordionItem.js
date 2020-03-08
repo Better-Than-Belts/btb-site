ed0c3917c0ef1186ec606904efe227b30fed0084
Vincent Carlino <vjcarlino22@gmail.com>
2020-03-08 15:02:57 -0400

    make cursor change on hover

--- src/components/Accordion/AccordionItem.js
+++ src/components/Accordion/AccordionItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { H3 } from '../../styles';
+import { H3, P } from '../../styles';
 import './accordion.css';
 import styled from 'styled-components';
 
@@ -25,7 +25,7 @@ class AccordionItem extends React.Component {
                 <AccordionItemTextWrapper onClick={this.onClick}><H3 className={"accordion" + (this.state.open ? " open": "")} >{this.props.title}</H3></AccordionItemTextWrapper>
                 <div className={"accordion-content" + (this.state.open ? " open" : "")}>
                     <AccordionItemTextWrapper>
-                        { this.props.children }
+                        <P>{ this.props.children }</P>
                     </AccordionItemTextWrapper>
                 </div>
             </AccordionItemWrapper>
@@ -38,6 +38,7 @@ class AccordionItem extends React.Component {
 const AccordionItemWrapper = styled.div`
     margin-top: 10px;
     border-top: solid 1px #c4c4c4;
+    cursor: pointer;
 `;
 
 const AccordionItemTextWrapper = styled.div`
