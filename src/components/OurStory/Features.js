fd91d79e7602e83fd1ab54166a033e400d4b49e0
Tina Wu <tinawu0603@hotmail.com>
2020-04-02 14:56:58 -0400

    added mobile

--- src/components/OurStory/Features.js
+++ src/components/OurStory/Features.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Section, H2, P, ImageContainer, MockImageOurStory } from '../../styles';
+import { device } from '../../device.js';
 
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida semper augue sagittis vulputate. Quisque lobortis pellentesque mi, eget placerat leo porttitor eget. Proin at volutpat justo, pretium finibus lorem. Curabitur congue consectetur enim ullamcorper aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit"
 
 const OurStoryFeatures = (props) => {
     return (
-        <Section>
+        <SectionContainer>
             <FeatureSection className="row">
-                <ImageContainer className="col-xs-12 col-xl-6">
+                <ImageContainer className="col-xs-12 col-md-6">
                     <MockImageOurStory />
                 </ImageContainer>
-                <div className="col-xs-12 col-xl-6">
+                <div className="col-xs-12 col-md-6">
                     <Title>It All Starts with an idea</Title>
                     <FeatureContent>{content}</FeatureContent>
                 </div>
             </FeatureSection>
             <FeatureSection className="row flex-md-row-reverse">
-                <ImageContainer className="col-xs-12 col-xl-6">
+                <ImageContainer className="col-xs-12 col-md-6">
                     <MockImageOurStory />
                 </ImageContainer>
-                <div className="col-xs-12 col-xl-6">
+                <div className="col-xs-12 col-md-6">
                     <Title>Designed with you in mind</Title>
                     <FeatureContent>{content}</FeatureContent>
                 </div>
             </FeatureSection>
             <FeatureSection className="row">
-                <ImageContainer className="col-xs-12 col-xl-6">
+                <ImageContainer className="col-xs-12 col-md-6">
                     <MockImageOurStory />
                 </ImageContainer>
-                <div className="col-xs-12 col-xl-6">
+                <div className="col-xs-12 col-md-6">
                     <Title>Looking forward</Title>
                     <FeatureContent>{content}</FeatureContent>
                 </div>
             </FeatureSection>
-        </Section>
+        </SectionContainer>
     );
 };
 
@@ -52,6 +53,19 @@ const FeatureSection = styled.div`
 
 const FeatureContent = styled(P)`
     line-height: 30px;
+`
+const SectionContainer = styled(Section)`
+    padding-left: 150px;
+    padding-right: 150px;
+
+    @media ${device.tablet} {
+        padding-left: 60px;
+        padding-right: 60px;
+    }
+    @media ${device.mobile} {
+        padding-left: 30px;
+        padding-right: 30px;
+    }
 `
 
 export default OurStoryFeatures; 
--- src/components/OurStory/OurBackground.js
+++ src/components/OurStory/OurBackground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { H2, P, BGGray, Section } from '../../styles';
+import { device } from '../../device.js';
 
 const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida semper augue sagittis vulputate. Quisque lobortis pellentesque mi, eget placerat leo porttitor eget. Proin at volutpat justo, pretium finibus lorem. Curabitur congue consectetur enim ullamcorper aliquam."
 
@@ -9,17 +10,31 @@ const content2 = "Mauris vehicula enim at diam pulvinar, at posuere est mattis. 
 const OurBackground = (props) => {
     return (
         <BGGray class="container">
-            <Section>
+            <SectionContainer>
                 <OurBackgroundTitle>Our Background</OurBackgroundTitle>
                 <OurBackgroundContent>{content}</OurBackgroundContent>
                 <br />
                 <OurBackgroundContent>{content2}</OurBackgroundContent>
-            </Section>
+            </SectionContainer>
         </BGGray>
     );
 };
 
 // Styles
+const SectionContainer = styled(Section)`
+    padding-left: 150px;
+    padding-right: 150px;
+    padding-top: 50px;
+    @media ${device.tablet} {
+        padding-left: 60px;
+        padding-right: 60px;
+    }
+    @media ${device.mobile} {
+        padding-left: 30px;
+        padding-right: 30px;
+    }
+`
+
 const OurBackgroundTitle = styled(H2)`
     padding-bottom: 30px;
 `
