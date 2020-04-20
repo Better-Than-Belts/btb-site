0d7c6b38e1d6a2270b9d9c5de3d899cff0b4c61f
Tina Wu <tinawu0603@hotmail.com>
2020-04-19 21:14:17 -0400

    add plp hifi

--- src/components/PLP/PLPProduct.js
+++ src/components/PLP/PLPProduct.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import { H3 } from '../../styles';
+import { H3, P } from '../../styles';
+import { device } from '../../device';
+import { Link } from 'react-router-dom';
 
 class PLPProduct extends React.Component {
     render() {
         return (
             <ProductDiv className="col-md-4 col-6">
-                {this.props.product.images.length ?
-                    <img src={this.props.product.images[0].src} alt={this.props.product.title} width="100%" /> : null}
-                <ProductName>{this.props.product.title}</ProductName>
-                <ProductPrice>{this.props.product.priceRange}</ProductPrice>
+                <PDPLink to={`/shop/${this.props.product.id}`}>
+                    {this.props.product.images.length ?
+                        <Image src={this.props.product.images[0].src} alt={this.props.product.title} /> : null}
+                    <ProductName>{this.props.product.title} - {this.props.product.variants[0].price}</ProductName>
+                </PDPLink>
             </ProductDiv>
         )
     }
@@ -18,17 +21,27 @@ class PLPProduct extends React.Component {
 
 export default PLPProduct;
 
-const ProductName = styled(H3)`
+const ProductName = styled(P)`
     font-weight: normal;
-    font-size: 16px;
-`
-
-const ProductPrice = styled(H3)`
-    font-weight: normal;
-    font-size: 16px;
+    padding: 10px 0px;
 `
 
 const ProductDiv = styled.div`
     padding-top: 10px;
     padding-bottom: 10px;
 `
+
+const PDPLink = styled(Link)`
+    color: #0C1527;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 515px;
+    object-fit: cover;
+    border: none;
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.15);
+    @media ${device.tablet} {
+        height: 350px;
+    }
+`;
