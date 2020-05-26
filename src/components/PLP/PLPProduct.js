86841819dd7fcc10989ea1d7c4c12c48316146d6
Vincent Carlino <vjcarlino22@gmail.com>
2020-05-25 20:15:24 -0400

    Missing import

--- src/components/PLP/PLPProduct.js
+++ src/components/PLP/PLPProduct.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { P } from '../../styles';
+import { P, Flex } from '../../styles';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
